abaf8f969cd1fd6401babdcd50f1cc6da73b4c6e
TheSudo <55331456+Josh-Tracy@users.noreply.github.com>
2024-05-24 14:24:24 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'The_Sudo',
     description: 'Kasm Workspaces Created by TheSudo',
-    icon: '/img/logo.svg',
+    icon: 'https://yt3.ggpht.com/yti/ANjgQV-GGV4fXu-4cOi2ucjyJYGlD8E4dSGlouGgwqt50F_dhis=s88-c-k-c0x00ffffff-no-rj',
     listUrl: 'https://TheSudoYT.github.io/kasm-registry/',
     contactUrl: 'https://github.com/TheSudoYT/kasm-registry/issues',
   },
